0e63f117839abe97fb94b12fbb4bc1703f6f587a
gpentinio23 <gpentinio@lions.molloy.edu>
2025-04-22 04:31:28 -0400

    test

---
+++ src/SnakeController.ts
@@ -0,0 +1,19 @@
+﻿import Snake from "./Snake";
+import WorldModel from "./Worldmodel";
+class SnakeController {
+    private snakeWorld: WorldModel;
+    private slitherer: Snake;
+    constructor(worldModel: WorldModel, snake: Snake) {
+        this.snakeWorld = worldModel;
+        this.slitherer = snake;
+    }
+
+    turnSnakeLeft(): void {
+        this.slitherer.turnLeft();
+    }
+
+    turnSnakeRight(): void {
+        this.slitherer.turnRight();
+    }
+
+}
